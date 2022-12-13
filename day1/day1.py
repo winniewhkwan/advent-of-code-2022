8278bcad84650a223a0e7c353c614439ea60b149
winniewkwan <winniekwan.wh@gmail.com>
2022-12-13 15:04:25 -0500

    Tab conversion

--- day1/day1.py
+++ day1/day1.py
@@ -3,15 +3,15 @@
 
 highestCount = 0
 with open('day1/input.txt') as file:
-  allLines = file.readlines()
-  currentCount = 0
+	allLines = file.readlines()
+	currentCount = 0
 
-  for line in allLines:
-    if line.strip():
-      currentCount += int(line)
-    else:
-      if currentCount > highestCount:
-        highestCount = currentCount
-      currentCount = 0
+	for line in allLines:
+		if line.strip():
+			currentCount += int(line)
+		else:
+			if currentCount > highestCount:
+				highestCount = currentCount
+			currentCount = 0
 
 print('Highest number of calories being carried is', highestCount)
--- day2/day2.py
+++ day2/day2.py
@@ -4,30 +4,30 @@
 from enum import Enum
 
 class ResultGuide(Enum):
-  LOSS = 0
-  DRAW = 3
-  WIN = 6
+	LOSS = 0
+	DRAW = 3
+	WIN = 6
 
 class OptionGuide(Enum):
-  A = 1 # Rock
-  B = 2 # Paper
-  C = 3 # Scissors
-  X = 1 # Rock
-  Y = 2 # Paper
-  Z = 3 # Scissors
+	A = 1 # Rock
+	B = 2 # Paper
+	C = 3 # Scissors
+	X = 1 # Rock
+	Y = 2 # Paper
+	Z = 3 # Scissors
 
 totalScore = 0
 with open('day2/input.txt') as file:
-  allLines = file.readlines()
-  for line in allLines:
-    opponent = OptionGuide[line.split()[0]].value
-    player = currentScore = OptionGuide[line.split()[1]].value
-    if opponent == player:
-      currentScore += ResultGuide.DRAW.value
-    elif player == OptionGuide.X.value and opponent == OptionGuide.C.value\
-      or player == OptionGuide.Y.value and opponent == OptionGuide.A.value\
-      or player == OptionGuide.Z.value and opponent == OptionGuide.B.value:
-        currentScore += ResultGuide.WIN.value
-    totalScore += currentScore
+	allLines = file.readlines()
+	for line in allLines:
+		opponent = OptionGuide[line.split()[0]].value
+		player = currentScore = OptionGuide[line.split()[1]].value
+		if opponent == player:
+			currentScore += ResultGuide.DRAW.value
+		elif player == OptionGuide.X.value and opponent == OptionGuide.C.value\
+			or player == OptionGuide.Y.value and opponent == OptionGuide.A.value\
+			or player == OptionGuide.Z.value and opponent == OptionGuide.B.value:
+				currentScore += ResultGuide.WIN.value
+		totalScore += currentScore
 
 print('Total score is', totalScore)
