e14071638baf6302688653772c3337d364b0b6f6
winniewkwan <winniekwan.wh@gmail.com>
2023-01-14 11:09:56 -0500

    Getting closer to figuring out day 2 bonus

--- day2/day2_bonus.py
+++ day2/day2_bonus.py
@@ -3,23 +3,40 @@
 
 from enum import Enum
 
+class OptionGuide(Enum):
+	A = 1 # Rock
+	B = 2 # Paper
+	C = 3 # Scissors
+
 class ResultGuide(Enum):
-	LOSS = 'X'
-	DRAW = 'Y'
-	WIN = 'Z'
+	LOSS = 0,
+	DRAW = 1,
+	WIN = 2
+
+class ResultMap(Enum):
+	A = ['B', 'A', 'C']
+	B = ['C', 'B', 'A']
+	C = ['A', 'C', 'B']
+
+def get_outcome(opponent_choice, result):
+	player_options = ResultMap[opponent_choice].value
+	player_choice = player_options[result]
+	return OptionGuide[player_choice].value
 
 total_score = 0
-with open('day2/input.txt') as file:
+with open('day2/test_input.txt') as file:
 	all_lines = file.readlines()
 	for line in all_lines:
-		result = line.split()[1]
-		current_score = 1
+		opponent, result = line.split()
+		current_score = 0
 
 		match result:
-			case ResultGuide.DRAW.value:
-				current_score += 3
-			case ResultGuide.WIN.value:
-				current_score += 6
+			case 'X': # Lose
+				current_score = 0 + get_outcome(opponent, 0)
+			case 'Y': # Draw
+				current_score = 3 + get_outcome(opponent, 1)
+			case 'Z': # Win
+				current_score = 6 + get_outcome(opponent, 2)
 
 		total_score += current_score
 
