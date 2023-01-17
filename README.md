d434fd62c6cc8ba3fd547d9b71a30a0deef21af3
winniewkwan <winniekwan.wh@gmail.com>
2023-01-16 22:22:38 -0500

    Solved day 3 part two

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ For those of you who don't know what Advent of Code is check it out [here](https
 |:---------:| :----------------------: | :-------------------------------: |
 | [1](day1) |  [Solved](day1/day1.py)  | [Solved](day1/day1_bonus.py)      |
 | [2](day2) |  [Solved](day2/day2.py)  | [Solved](day2/day2_bonus.py)      |
-| [5](day5) |  [Solved](day5/day5.py)  | [In Progress](day3/day3_bonus.py) |
-| [3](day3) |  [Solved](day3/day3.py)  |
+| [3](day3) |  [Solved](day3/day3.py)  | [Solved](day3/day3_bonus.py)      |
 | [4](day4) |  [Solved](day4/day4.py)  |
+| [5](day5) |  [Solved](day5/day5.py)  |
 | [6](day6) |  [Solved](day6/day6.py)  |
 | [7](day7) |  [Paused](day7/day7.py)  |
--- day3/day3_bonus.py
+++ day3/day3_bonus.py
@@ -1,26 +1,23 @@
 # Day 3 info here https://adventofcode.com/2022/day/3
 # Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
 
-with open('day3/test_input.txt') as file:
-	sameItemTypeTotal = 0
-	allLines = file.readlines()
+with open('day3/input.txt') as file:
+	badge_total = 0
+	all_lines = file.readlines()
 
-	# Need to group lines by 3
-	# Find common type between lines and store
-	# Find priority number and get total
+	current_group = []
+	for line in all_lines:
+		current_group.append(line)
+		if len(current_group) == 3:
+			pack1, pack2, pack3 = current_group
 
-	for line in allLines:
-		# Split text in half - the '//' divides the number and rounds down
-		pocketOne = line[:len(line)//2]
-		pocketTwo = line[len(line)//2:]
-
-		for item in pocketOne:
-			if item in pocketTwo:
-				if (item.islower()):
-					# Use ascii to determine priority number for the item
-					sameItemTypeTotal += ord(item) - 96
-				else:
-					sameItemTypeTotal += ord(item) - 38
-				# Leave loop after similar item has been discovered
-				break
-	print('The sum of the item priorities is', sameItemTypeTotal)
+			for item_type in pack1:
+				if item_type in pack2 and item_type in pack3:
+					if (item_type.islower()):
+						# Use ascii to determine priority number for the item
+						badge_total += ord(item_type) - 96
+					else:
+						badge_total += ord(item_type) - 38
+					break
+			current_group = []
+	print('The sum of the group priorities is', badge_total)
