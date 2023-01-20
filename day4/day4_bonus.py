902bee7cb001ffdbd8c393692f6ec26a18adc5b7
winniewkwan <winniekwan.wh@gmail.com>
2023-01-20 08:42:03 -0500

    Updated day 4 part two

--- day4/day4_bonus.py
+++ day4/day4_bonus.py
@@ -9,9 +9,21 @@ with open('day4/test_input.txt') as file:
 
 	for line in all_lines:
 		num_array = [int(num) for num in re.split(r'[,-]', line)]
+		# [2-8,3-7]
+		#  0,1,2,3
 
-		if num_array[0] >= num_array[2] and num_array[1] <= num_array[3]\
-			or num_array[0] <= num_array[2] and num_array[1] >= num_array[3]:
-				total_overlaps += 1
+		# if [0] is within [2] and [3]
+		# or if [1] is within [2] and [3]
+		# or [2] is within [0] and [1]
+		# or [3] is within [0] and [1]
+		# Also find a more interesting (better) way to solve this
+
+		if num_array[0] >= num_array[2] and num_array[0] <= num_array[3]\
+			or num_array[1] >= num_array[2] and num_array[1] <= num_array[3]:
+			total_overlaps += 1
+
+		# if num_array[0] >= num_array[2] and num_array[1] <= num_array[3]\
+		# 	or num_array[0] <= num_array[2] and num_array[1] >= num_array[3]:
+		# 		total_overlaps += 1
 
 	print('Total overlaps are', total_overlaps)
